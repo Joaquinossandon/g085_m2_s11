640956a2989609013839c5ce4d96dcdb0cf6d518
Joaquin Ossandón Gómez <joaquin.ossandon@inacapmail.cl>
2024-07-22 13:15:17 -0400

    Se agrega ruleta rusa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,48 @@
-document.querySelector("h1").textContent = "Estoy cambiando el texto denuevo"
+const participantes = [
+  "Carlos Nuñez",
+  "Claudio Nuñez",
+  "Dharma Reyes",
+  "Felipe rivera",
+  "Felipe Urtubia",
+  "Fernando díaz",
+  "Gabriel Alarcón",
+  "Gustavo Pinto",
+  "James Moreno",
+  "Jorge Paillan",
+  "Luis Abarca",
+  "Maicon Manriquez",
+  "Manuel Herrera",
+  "Marcelo Emmott",
+  "Maximiliano Gutierrez",
+  "Michael Jamet",
+  "Miguel Serrano",
+  "Miguel Durán",
+  "Natalia Campos",
+  "Pablo Salcedo",
+  "Paz Gutierrez",
+  "Pedro Valdebenito",
+  "Vicente Rosales",
+  "Yanara Catrilelbún",
+  "Beptson Cortes",
+  "Luis Araya",
+  "Carlos Roa",
+  "Leonardo Quiroga",
+  "Juan Pablo Quesada",
+  "Manuel Gatica",
+];
+
+const button = document.querySelector("#ruleta");
+
+const getRandom = (array) => {
+  const randomNumber = Math.floor(Math.random() * array.length);
+  const valorSeleccionado = array[randomNumber];
+  return valorSeleccionado;
+};
+
+const renderRandom = () => {
+  const persona = getRandom(participantes);
+  const h1Seleccion = document.querySelector("#seleccion");
+  h1Seleccion.textContent = persona;
+};
+
+button.addEventListener("click", renderRandom);
